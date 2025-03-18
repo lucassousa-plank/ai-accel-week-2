510e33f5fb5f0e922e65dabe312b2b2ba134babd
lucassousa-plank <lucas.sousa@joinplank.com>
2025-03-18 18:17:33 -0300

    Add map-reduce example and improve readme file

---
+++ apps/agents/src/custom-agents/map-reduce/prompts.ts
@@ -0,0 +1,11 @@
+/**
+ * Default prompts.
+ */
+
+export const subjectsPrompt = `Generate a comma separated list of between 2 and 5 examples related to: {topic}.`
+
+export const jokePrompt = `Generate a joke about {subject}`
+
+export const bestJokePrompt = `Below are a bunch of jokes about {topic}. Select the best one! Return the ID (index) of the best one.
+
+{jokes}`
